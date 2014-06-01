78328b608b6b9ef7acf6f9dfbeb9500b66d2d743
Michael Kelly <mkelly@mozilla.com>
2014-06-01 17:40:50 -0400

    Let’s make sound happen.

--- musaks.js
+++ musaks.js
@@ -1,8 +1,9 @@
 ;(function() {
     'use strict';
 
-    var paper = Raphael(document.getElementById('canvas'), 800, 600);
-    var nodes = [];
+    var audioCtx = new AudioContext();
+    var paper = new Raphael(document.getElementById('canvas'), 800, 600);
+    var sourceNodes = [];
     var currentTool = 'move';
     var currentConnection = null;
 
@@ -23,12 +24,35 @@
         return line;
     };
 
-    function VisualNode(x, y, node) {
-        this.circle = paper.circle(x, y, 10).attr('fill', '#F00');
+    Raphael.fn.circleConnection = function(startCircle, endCircle) {
+        var connection = this.line(startCircle.attr('cx'), startCircle.attr('cy'),
+                                   endCircle.attr('cx'), endCircle.attr('cy'));
+        connection.startCircle = startCircle;
+        connection.endCircle = endCircle;
+        connection.redraw = function() {
+            this.attr('path', linePath(
+                this.startCircle.attr('cx'), this.startCircle.attr('cy'),
+                this.endCircle.attr('cx'), this.endCircle.attr('cy'))
+            );
+        };
+
+        return connection;
+    };
+
+    function VisualNode(x, y, audioNode, color) {
+        var self = this;
+        this.connections = [];
+        this.audioNode = audioNode;
+
+        this.circle = paper.circle(x, y, 10).attr('fill', color || '#F00');
+        this.circle.visualNode = this;
         this.circle.isNode = true;
         this.circle.drag(function(dx, dy) {
             if (currentTool == 'move') {
                 this.attr({cx: x + dx, cy: y + dy});
+                self.connections.forEach(function(connection) {
+                    connection.redraw();
+                });
             } else if (currentTool == 'connect') {
                 currentConnection.endX = x + dx;
                 currentConnection.endY = y + dy;
@@ -42,20 +66,27 @@
                 currentConnection = paper.line(x, y, x, y);
             }
         }, function() {
-            var elements = paper.getElementsByPoint(currentConnection.endX,
-                                                    currentConnection.endY);
-            if (elements) {
-                var topNode = null;
-                for (var k = 0; k < elements.length; k++) {
-                    if (element.isNode) {
-                        topNode = element;
-                        break;
+            if (currentConnection) {
+                var elements = paper.getElementsByPoint(currentConnection.endX,
+                                                        currentConnection.endY);
+                if (elements) {
+                    var topNode = null;
+                    for (var k = 0; k < elements.length; k++) {
+                        var element = elements[k];
+                        if (element.isNode) {
+                            topNode = element;
+                            break;
+                        }
+                    }
+
+                    if (topNode) {
+                        self.connections.push(paper.circleConnection(this, topNode));
+                        self.audioNode.connect(topNode.visualNode.audioNode);
                     }
                 }
 
-                if (topNode) {
-
-                }
+                currentConnection.remove();
+                currentConnection = null;
             }
         });
     }
@@ -63,7 +94,13 @@
     paper.canvas.onclick = function(e) {
         e.preventDefault();
         if (currentTool == 'sourcenode') {
-            nodes.append(new VisualNode(e.pageX, e.pageY, null));
+            var oscillator = audioCtx.createOscillator();
+            oscillator.type = 'square';
+            var gain = audioCtx.createGain();
+            oscillator.connect(gain);
+            gain.gain.value = 0;
+            oscillator.start(0);
+            sourceNodes.push(new VisualNode(e.pageX, e.pageY, gain));
         }
     };
 
@@ -74,4 +111,19 @@
         $tools.removeClass('selected');
         $newTool.addClass('selected');
     });
+
+    $('#play').click(function() {
+        sourceNodes.forEach(function(sourceNode) {
+            sourceNode.audioNode.gain.value = 1;
+        });
+    });
+
+    $('#stop').click(function() {
+        sourceNodes.forEach(function(sourceNode) {
+            sourceNode.audioNode.gain.value = 0;
+        });
+    });
+
+    // Create node for destination.
+    var destinationNode = new VisualNode(700, 300, audioCtx.destination, '#66F');
 })();
